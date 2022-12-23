f33731c3e573d055736fd07f6a23588b1f6b03b3
Rafael Mileris <rafaelmileris15@gmail.com>
2022-12-23 15:21:47 -0300

    Adicionando novas anotações

---
+++ Aulas/COUNT()_AVG()_SUM().sql
@@ -0,0 +1,22 @@
+/*
+COUNT() - Vai retornar o numero de linhas que correspondem a condição especificada
+AVG() - Retorna o valor médio de uma coluna numerica
+SUM() - Retorna a soma de todos os valores de uma coluna numerica
+*/
+
+
+#Utilizando o COUNT()
+SELECT COUNT(<coluna>)
+FROM <nome_tabela>
+WHERE <condição>
+
+#Utilizando o AVG()
+SELECT AVG(<coluna>)
+FROM <nome_tabela>
+WHERE <condição>
+
+#Utilizando o SUM()
+SELECT SUM(<coluna>)
+FROM <nome_tabela>
+WHERE <condição>
+
